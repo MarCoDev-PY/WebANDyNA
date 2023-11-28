ab1e0b53a822e44370c736980f6e5780250182ee
MarCoDev <superanthony0493@gmail.com>
2023-11-27 20:05:05 -0400

    AGREGACION CONTEXT

---
+++ Context/MiContext.cs
@@ -0,0 +1,20 @@
+﻿using ANDyNAwebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ANDyNAwebApp.Context
+{
+    public class MiContext : DbContext
+    {
+        public MiContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        //REVISAR SI LA ES NECESARIO UNA BASE DE DATOS PARA LOS CLIENTES
+        public DbSet<Cliente> Cliente { get; set; }
+
+        //REVISAR SI LAS RESERVAS TAMBIEN ESTARAN EN LA BASE DE DATOS
+        //public DbSet<Reserva> reservas { get; set; }
+    }
+}
